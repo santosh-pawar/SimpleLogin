dacea8375e935e96e7b7ac489af45aa194ccd892
Santosh Pawar <sanpaw.cs@gmail.com>
2015-04-13 20:55:49 -0500

    Initial Commit

---
+++ SimpleLogin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleLogin
+//
+//  Created by Santosh Pawar on 4/13/15.
+//  Copyright (c) 2015 Santosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
